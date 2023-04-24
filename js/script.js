ba0ecb3ddbc394f9dd1f3d3bb05e2d2960d51ed4
Collin R <collin.codes.hard@gmail.com>
2023-04-24 14:11:16 -0500

    Created method for animal playtime

--- js/script.js
+++ js/script.js
@@ -7,10 +7,23 @@ const createPet = function(name, species) {
         name: name,
         species: species,
         isTired: 5, //1 = refreshed, 10 = exhausted
+        //create method for sleep
         sleep: function() {
             console.log(`${name} needs a nap. Zzz...`);
             this.isTired = 1;
+        },
+        //create method for playtime
+        play: function() {
+            if (this.isTired === 10) {
+                console.log("Too tired to play.");
+                this.sleep();
+            }
+             else {
+                console.log(`Yay! ${this.name} loves to play!`);
+                this.isTired += 1;
+            }
         }
     }
+    return pet;
 };
 
