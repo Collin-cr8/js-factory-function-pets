9347936be5e816e57b085c882258f0320a8f00c3
Collin R <collin.codes.hard@gmail.com>
2023-04-24 14:02:46 -0500

    Created factory function to create a pet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
+//Factory Function
+const createPet = function(name, species) {
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5, //1 = refreshed, 10 = exhausted
+        sleep: function() {
+            console.log(`${name} needs a nap. Zzz...`);
+            this.isTired = 1;
+        }
+    }
+};
+
